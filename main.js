b4331dc2b90c9c515d6d9819d081c08c26e5b430
jiwi-cloud <ewinz.evgeny@mail.ru>
2024-11-27 03:19:22 +0300

    created: added parallax effect for block Mail-subscribe and finished block Reviews

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const hotelOverviewSlider = new Swiper('.hotel-overview__slider', {
     direction: 'horizontal', // Направление слайдов
     loop: true, // Зацикливание слайдов
-    effect: 'coverflow',
+    effect: 'cube',
     // Пагинация
     pagination: {
         el: '.swiper-pagination',
@@ -23,3 +23,37 @@ const hotelOverviewSlider = new Swiper('.hotel-overview__slider', {
     slidesPerView: 1, // Количество видимых слайдов
     spaceBetween: 20, // Расстояние между слайдами
 });
+
+const reviewSlider = new Swiper('.reviews-swipercontainer', {
+    direction: 'horizontal', // Направление слайдов
+    loop: true, // Зацикливание слайдов
+    effect: 'slide',
+    // Пагинация
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true, // Делает точки пагинации кликабельными
+    },
+
+    // Навигационные стрелки
+    navigation: {
+        nextEl: '.reviews__arrow--right', // Кнопка "вперед"
+        prevEl: '.reviews__arrow--left', // Кнопка "назад"
+    },
+
+    // Прокрутка (если нужна)
+    scrollbar: {
+        el: '.swiper-scrollbar', // Полоса прокрутки
+    },
+
+    // Дополнительные параметры
+    slidesPerView: 1, // Количество видимых слайдов
+    spaceBetween: 20, // Расстояние между слайдами
+});
+
+
+document.addEventListener('scroll', function () {
+    const parallaxBg = document.querySelector('.img-newsletter');
+    const scrollPosition = window.scrollY;
+    parallaxBg.style.transform = `translateY(${scrollPosition * 0.5}px)`; // Чем меньше коэффициент, тем медленнее движение
+});
+
